ad7466bc9813f7bc71b3bec5a5baf6ccb38acc5a
Will Wolf <williamabrwolf@gmail.com>
2022-01-06 11:09:05 -0600

    compute staker yield from inflation and perc_staked

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
+import os
 import time
 
 import numpy as np
 from ruamel.yaml import YAML
 import streamlit as st
 
-from model import s_inflation
+from model import (
+  compute_staker_yield,
+  p_inflation,
+  p_perc_staked,
+  s_inflation,
+  s_perc_staked,
+  s_staker_yield,
+)
 from utils import CadCadSimulationBuilder
 
-import os
 
 CONFIG_PATH = os.path.join(
   os.path.dirname(__file__),
@@ -18,11 +25,22 @@ C = CONSTANTS = YAML(typ='safe').load(open(CONFIG_PATH))
 
 # Define sidebar
 
-st.sidebar.markdown('## Set economic parameters')
+st.sidebar.markdown('## Economic parameters')
 
-base_infl_rate      = st.sidebar.slider("Base inflation rate", 0., 20., C['base_inflation_rate'] * 100, .5) / 100
-dis_infl_rate       = st.sidebar.slider("Disinflation rate", -20., 0., C['disinflation_rate'] * 100, .5) / 100
-long_term_infl_rate = st.sidebar.slider("Long-term inflation rate", 0., 20., C['long_term_inflation_rate'] * 100, .5) / 100
+st.sidebar.markdown('### Staker')
+
+init_perc_staked    = st.sidebar.slider("Initial percentage staked", 0., 100., C['initial_fraction_staked'] * 100., .5) / 100
+
+st.sidebar.markdown('### Inflation')
+
+base_infl_rate      = st.sidebar.slider("Base inflation rate", 0., 20., C['base_inflation_rate'] * 100., .5) / 100
+dis_infl_rate       = st.sidebar.slider("Disinflation rate", -20., 0., C['disinflation_rate'] * 100., .5) / 100
+long_term_infl_rate = st.sidebar.slider("Long-term inflation rate", 0., 20., C['long_term_inflation_rate'] * 100., .5) / 100
+
+st.sidebar.markdown('### Validator')
+
+vdtr_comm_perc      = st.sidebar.slider("Validator commission percentage", 0., 100., C['validator_commission_fraction'] * 100., .5) / 100
+vdtr_uptime_freq    = st.sidebar.slider("Validator uptime frequency", 0., 100., C['validator_uptime_frequency'] * 100., .5) / 100
 
 st.sidebar.markdown('## Simulate mint')
 
@@ -33,9 +51,9 @@ progress_bar = st.sidebar.progress(0)
 errors = []
 
 if long_term_infl_rate > base_infl_rate:
-    errors.append(
-        lambda: st.error('Long-term inflation rate must be <= base inflation rate')
-    )
+  errors.append(
+      lambda: st.error('Long-term inflation rate must be <= base inflation rate')
+  )
 
 # Run simulation
 
@@ -46,15 +64,26 @@ simulation = CadCadSimulationBuilder.build(
         'base_infl_rate': base_infl_rate,
         'dis_infl_rate': dis_infl_rate,
         'long_term_infl_rate': long_term_infl_rate,
+        'vdtr_comm_perc': vdtr_comm_perc,
+        'vdtr_uptime_freq': vdtr_uptime_freq,
     },
     initial_state={
-        'inflation': base_infl_rate
+        'inflation': base_infl_rate,
+        'perc_staked': init_perc_staked,
+        'staker_yield': compute_staker_yield(
+          base_infl_rate, vdtr_uptime_freq, vdtr_comm_perc, init_perc_staked
+        )
     },
     partial_state_update_blocks=[
         {
-            'policies': {},
+            'policies': {
+              'inflation': p_inflation,
+              'perc_staked': p_perc_staked,
+            },
             'variables': {
-                'inflation': s_inflation,
+              'inflation': s_inflation,
+              'perc_staked': s_perc_staked,
+              'staker_yield': s_staker_yield,
             }
         }
     ],
--- app/model.py
+++ app/model.py
@@ -1,6 +1,10 @@
-def s_inflation(params, substep, state_history, previous_state, policy_input):
+def compute_staker_yield(inflation, uptime, commission, perc_staked):
+  return inflation * uptime * (1 - commission) / perc_staked
+
+
+def p_inflation(params, substep, state_history, previous_state):
     """
-    Update token inflation.
+    Compute the % of token inflation that will occur over upcoming timestep.
 
     If we start with 100 tokens, and inflation is 7%, then next year
     the system will have 107 tokens, where new tokens are distributed
@@ -10,10 +14,43 @@ def s_inflation(params, substep, state_history, previous_state, policy_input):
     base_rate = params['base_infl_rate']
     grow_rate = params['dis_infl_rate']
     ltr       = params['long_term_infl_rate']
-    timestep  = len(state_history)
+    timestep  = previous_state['timestep'] + 1
 
     inflation = max(
       base_rate * (1 + grow_rate)**timestep,
       ltr
     )
-    return 'inflation', inflation
+    return {'inflation': inflation}
+
+
+def p_perc_staked(params, substep, state_history, previous_state):
+    """
+    Computer percentage of total SOL that will be staked in upcoming timestep.
+
+    New SOL issued via inflation is awarded to stakers, and is *automatically
+    restaked.*
+    """
+    perc_staked = previous_state['perc_staked']
+    return {'perc_staked': perc_staked}
+
+
+def s_inflation(params, substep, state_history, previous_state, policy_input):
+  return 'inflation', policy_input['inflation']
+
+
+def s_perc_staked(params, substep, state_history, previous_state, policy_input):
+  return 'perc_staked', policy_input['perc_staked']
+
+
+def s_staker_yield(params, substep, state_history, previous_state, policy_input):
+    """
+    Update the staker yield.
+    """
+    params,   = params  # I don't know why this is necessary.
+    commission = params['vdtr_comm_perc']
+    uptime = params['vdtr_uptime_freq']
+    inflation = policy_input['inflation']
+    perc_staked = policy_input['perc_staked']
+
+    staker_yield = compute_staker_yield(inflation, uptime, commission, perc_staked)
+    return 'staker_yield', staker_yield
