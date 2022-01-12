fd3fb405c93623f8fe211e2ff6b7fa2c792cd82e
Will Wolf <williamabrwolf@gmail.com>
2022-01-12 16:13:41 -0500

    move description into separate file

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ import numpy as np
 import streamlit as st
 
 from chart import (
-  StakePropensityChart,
   PercStakedAltairChart,
   StakerYieldAltairChart,
   DilutionAltairChart,
   ValuationAltairChart,
 )
+from description import description
 from model import (
   compute_staker_yield,
   compute_unstaked_dilution,
@@ -190,42 +190,7 @@ st.image("app/assets/logo.png")
 
 with st.expander("See description"):
 
-  st.markdown('## Description')
-
-  st.write("""
-  In Solana, new SOL are minted on a given inflation schedule. These SOL are then distributed to those who "stake" their existing coins: participating in, or delegating to a validator who participates in, the validator of transactions on the Solana blockchain.
-
-  In this vein, staking SOL has a positive, dynamic yield. Conversely, the value of unstaked SOL is continuously diluted.
-
-  For more information on this system, please refer to the [Solana Economics Overview](https://docs.solana.com/economics_overview).
-  """)
-
-  st.markdown('## Simulation')
-
-  st.write("""
-
-  Below, we simulate this process. On the left, you can choose the "Staked" and "Unstaked" policy: the logic used to update ones strategy in this game.
-
-  In the *proactive* policy, we define a participant's propensity to change their behavior varies with the staker yield in the previous timestep.
-  """)
-
-  st.altair_chart(
-    StakePropensityChart.build(yield_location, yield_scale),
-    use_container_width=True
-  )
-
-  st.write("""
-  In the *constant* policy, a participant's behavior remains constant throughout.
-  """)
-
-  st.markdown('## About')
-
-  st.write("""
-  This work is *not* affiliated with the official Solana project in any way.
-
-  * [Author](https://willwolf.io/)
-  * [Code](https://github.com/cavaunpeu/solana-economics)
-  """)
+  description(yield_location, yield_scale)
 
 st.markdown('## Results')
 
